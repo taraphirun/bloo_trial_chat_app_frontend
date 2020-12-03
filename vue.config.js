fa2242cf85a7f47b9b3964e34aa8ed27412bd9d4
taraphirun <phiruntara@gtech.software>
2020-12-04 00:39:44 +0700

    - add pwa support [test working on android]

--- vue.config.js
+++ vue.config.js
@@ -17,4 +17,19 @@ module.exports = {
       enableEngine: true,
     },
   },
+  pwa: {
+    name: "My Chat App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      // ...other Workbox options...
+    },
+  },
 };
